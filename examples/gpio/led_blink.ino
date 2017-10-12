0ed47e479b7210e6d26d7838ca377023efa07b63
Deep Dhillon <deep.dhill6@gmail.com>
2017-10-11 22:10:35 -0400

    travis setup and an example folder added with gpio example

---
+++ examples/gpio/led_blink.ino
@@ -0,0 +1,19 @@
+
+#include <Arduino.h>
+#include <gpio/GPIO.h>
+
+GPIO<BOARD::D13> ledPin;
+
+void setup(){
+    ledPin.output();
+}
+
+void loop(){
+    ledPin = HIGH;
+
+    delay(1000);
+
+    ledPin = LOW;
+
+    delay(1000);
+}
